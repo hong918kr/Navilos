e7a3707223f8361aae8e61538035c3e5961a306b
donh-house-desktop <hong918kr@gmail.com>
2025-01-19 16:00:30 -0800

    ~5.1 not printed well

--- boot/Main.c
+++ boot/Main.c
@@ -1,8 +1,22 @@
 #include "stdint.h"
 
-void main (void)
-{
-    uint32_t* dummyAddr = (uint32_t*)(1024*1024*100);
-    *dummyAddr = sizeof(long);
+#include "HalUart.h"
 
+static void Hw_init(void);
+
+void main(void)
+{
+    Hw_init();
+
+    uint32_t i = 100;
+    while(i)
+    {
+        Hal_uart_put_char('S');
+        --i;
+    }
+}
+
+static void Hw_init(void)
+{
+    Hal_uart_init();
 }
